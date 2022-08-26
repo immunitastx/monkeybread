9fb1a1ecf13e2ca56ec45c38133811bc9d226210
Dillon Scott <dillonscott.2024@gmail.com>
2022-08-26 15:37:44 -0400

    add to favicon

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ pygments_style = 'sphinx'
 html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
 html_logo = '_static/logo.png'
+html_favicon = '_static/logo.png'
 html_theme_options = {
     "logo_only": True,
     "display_version": False,
