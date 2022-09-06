d70ff8b5847e29ea6b1ef648789057b06a5eb093
Dillon Scott <dillonscott.2024@gmail.com>
2022-09-06 13:06:07 -0400

    add to module path

--- monkeybread/plot/__init__.py
+++ monkeybread/plot/__init__.py
@@ -2,3 +2,4 @@
 from ._kernel_density import kernel_density
 from ._shortest_distances import shortest_distances
 from ._cell_contact import cell_contact
+from ._cell_transcript_proximity import cell_transcript_proximity
