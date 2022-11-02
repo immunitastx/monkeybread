2e52e61fa394ad11bbf01c86ebfdab7041f4337a
Dillon Scott <dillonscott.2024@gmail.com>
2022-11-02 16:05:19 -0400

    Add tests for unique vs total contact

--- tests/test_contact.py
+++ tests/test_contact.py
@@ -8,6 +8,14 @@ def assert_total_contact(adata, group1, group2, expected_num, radius = None, **k
     observed_num = sum(len(v) for v in observed_contact.values())
     assert observed_num == expected_num
 
+def assert_unique_contact(adata, group1, group2, expected_num, radius = None, **kwargs):
+    observed_contact = mb.calc.cell_contact(adata, "cell_type", group1, group2,
+                                            radius = radius, **kwargs)
+    observed_num = sum(len(v) for v in observed_contact.values()) - 0.5 * \
+        sum(0 if k not in group2 else sum(v in group1 for v in values) for k, values in
+            observed_contact.items())
+    assert observed_num == expected_num
+
 
 def test_dense_contact_radius_0(dense_sample):
     assert_total_contact(dense_sample, "DC", "T", 0, radius = 0)
@@ -33,6 +41,7 @@ def test_dense_contact_radius_reflexivity(dense_sample):
 
 def test_dense_contact_radius_self_count(dense_sample):
     assert_total_contact(dense_sample, "DC", "DC", 2, radius = 1)
+    assert_unique_contact(dense_sample, "DC", "DC", 1, radius = 1)
 
 
 def test_dense_auto_radius_calculation(dense_sample):
@@ -40,7 +49,8 @@ def test_dense_auto_radius_calculation(dense_sample):
 
 
 def test_dense_group_contact(dense_sample):
-    assert_total_contact(dense_sample, ["DC", "T"], "T", 6, radius = 1)
+    assert_total_contact(dense_sample, ["DC", "T"], "T", 8, radius = 1)
+    assert_unique_contact(dense_sample, ["DC", "T"], "T", 6, radius = 1)
 
 
 def test_dense_insignificance(dense_sample):
@@ -83,6 +93,7 @@ def test_sparse_contact_radius_reflexivity(sparse_sample):
 
 def test_sparse_contact_radius_self_count(sparse_sample):
     assert_total_contact(sparse_sample, "T", "T", 4, radius = 2)
+    assert_unique_contact(sparse_sample, "T", "T", 2, radius = 2)
 
 
 def test_sparse_auto_radius_calculation(sparse_sample):
