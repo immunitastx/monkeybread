e8dcfa4135e851d4431ab952317a1e372ace2837
Dillon Scott <dillonscott.2024@gmail.com>
2022-12-13 14:04:28 -0500

    Add sample data with specified expression for lig-rec testing

--- tests/calc_stat/test_ligand_receptor.py
+++ tests/calc_stat/test_ligand_receptor.py
@@ -3,31 +3,28 @@ import numpy as np
 import monkeybread as mb
 
 
-def test_ligand_receptor_score(dense_sample):
-    contacts = mb.calc.cell_contact(dense_sample, groupby="cell_type", group1="ct1", group2="ct2", radius=1)
-    lr_scores = mb.calc.ligand_receptor_score(dense_sample, contacts, [("A", "B"), ("C", "D")])
+def test_ligand_receptor_score(sample_with_expression):
+    contacts = mb.calc.cell_contact(sample_with_expression, groupby="cell_type", group1="ct1", group2="ct2", radius=1)
+    lr_scores = mb.calc.ligand_receptor_score(sample_with_expression, contacts, [("A", "B"), ("A", "C")])
 
-    expected_ab_score = (np.sqrt(0.64 * 3.1) + np.sqrt(7.39 * 3.1) + np.sqrt(7.39 * 2.5) + np.sqrt(6.4 * 4.66)) / 4
-    expected_cd_score = (np.sqrt(4.06 * 5.21) + np.sqrt(7.19 * 5.21) + np.sqrt(7.19 * 6.47) + np.sqrt(0.18 * 6.46)) / 4
+    expected_ab_score = np.sum(np.sqrt([i * i for i in range(10)])) / 10
+    expected_ac_score = np.sum(np.sqrt([i * (10 - i) for i in range(10)])) / 10
 
     assert np.allclose(lr_scores[("A", "B")], expected_ab_score, atol=0.01)
-    assert np.allclose(lr_scores[("C", "D")], expected_cd_score, atol=0.01)
+    assert np.allclose(lr_scores[("A", "C")], expected_ac_score, atol=0.01)
 
 
-def test_ligand_receptor_score_insignificance(dense_sample):
-    contacts = mb.calc.cell_contact(dense_sample, groupby="cell_type", group1="ct1", group2="ct2", radius=1)
-    lr_scores = mb.calc.ligand_receptor_score(dense_sample, contacts, [("A", "B"), ("C", "D")])
-    lr_stat_scores = mb.stat.ligand_receptor_score(dense_sample, contacts, lr_scores, n_perms=1000)
+def test_ligand_receptor_score_insignificance(sample_with_expression):
+    contacts = mb.calc.cell_contact(sample_with_expression, groupby="cell_type", group1="ct1", group2="ct2", radius=1)
+    lr_scores = mb.calc.ligand_receptor_score(sample_with_expression, contacts, ("A", "C"))
+    lr_stat_scores = mb.stat.ligand_receptor_score(sample_with_expression, contacts, lr_scores, n_perms=1000)
 
-    assert lr_stat_scores[("A", "B")][1] > 0.50
-    assert lr_stat_scores[("C", "D")][1] > 0.50
+    assert lr_stat_scores[("A", "C")][1] > 0.50
 
 
-def test_ligand_receptor_score_significance(dense_sample):
-    contacts = mb.calc.cell_contact(dense_sample, groupby="cell_type", group1="ct1", group2="ct2", radius=1)
-    lr_scores = mb.calc.ligand_receptor_score(dense_sample, contacts, [("G", "I"), ("K", "I"), ("L", "I")])
-    lr_stat_scores = mb.stat.ligand_receptor_score(dense_sample, contacts, lr_scores, n_perms=1000)
+def test_ligand_receptor_score_significance(sample_with_expression):
+    contacts = mb.calc.cell_contact(sample_with_expression, groupby="cell_type", group1="ct1", group2="ct2", radius=1)
+    lr_scores = mb.calc.ligand_receptor_score(sample_with_expression, contacts, ("A", "B"))
+    lr_stat_scores = mb.stat.ligand_receptor_score(sample_with_expression, contacts, lr_scores, n_perms=1000)
 
-    assert lr_stat_scores[("G", "I")][1] < 0.2
-    assert lr_stat_scores[("K", "I")][1] < 0.2
-    assert lr_stat_scores[("L", "I")][1] < 0.2
+    assert lr_stat_scores[("A", "B")][1] < 0.05
--- tests/plot/test_ligand_receptor.py
+++ tests/plot/test_ligand_receptor.py
@@ -1,5 +1,3 @@
-import itertools
-
 import numpy as np
 
 import monkeybread as mb
@@ -9,11 +7,11 @@ LIGAND_RECEPTOR_ROOT = ROOT / "ligand_receptor"
 LIGAND_RECEPTOR_FIGS = FIGS / "ligand_receptor"
 
 
-def test_ligand_receptor_scatter(dense_sample, image_comparer):
+def test_ligand_receptor_scatter(sample_with_expression, image_comparer):
     np.random.seed(0)
-    contacts = mb.calc.cell_contact(dense_sample, groupby="cell_type", group1="ct1", group2="ct2", radius=1)
-    lr_scores = mb.calc.ligand_receptor_score(dense_sample, contacts, list(itertools.product("GHKL", "ABCI")))
-    lr_stat_scores = mb.stat.ligand_receptor_score(dense_sample, contacts, lr_scores)
+    contacts = mb.calc.cell_contact(sample_with_expression, groupby="cell_type", group1="ct1", group2="ct2", radius=1)
+    lr_scores = mb.calc.ligand_receptor_score(sample_with_expression, contacts, [("A", "B"), ("A", "C")])
+    lr_stat_scores = mb.stat.ligand_receptor_score(sample_with_expression, contacts, lr_scores)
     mb.plot.ligand_receptor_scatter(lr_scores, lr_stat_scores, show=False)
 
     save_and_compare_images = image_comparer(LIGAND_RECEPTOR_ROOT, LIGAND_RECEPTOR_FIGS, tol=15)
