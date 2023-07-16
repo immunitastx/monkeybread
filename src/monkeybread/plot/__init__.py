439a7cc67c2076e295f9fc9f9f3e3ba75ebc2c57
mbernstein <mbernstein@immunitastx.com>
2023-07-16 21:17:18 +0000

    Implemented ligand-receptor embedding plot

--- src/monkeybread/plot/__init__.py
+++ src/monkeybread/plot/__init__.py
@@ -7,6 +7,6 @@ from ._cell_neighbors import (
 from ._cell_transcript_proximity import cell_transcript_proximity
 from ._embedding_other import embedding_filter, embedding_zoom
 from ._kernel_density import kernel_density, location_and_density
-from ._ligand_receptor import ligand_receptor_scatter
 from ._shortest_distances import shortest_distances, shortest_distances_pairwise
 from ._number_neighbors import number_of_neighbors
+from ._ligand_receptor import ligand_receptor_embedding, ligand_receptor_embedding_zoom, ligand_receptor_scatter
