c43f7ed6bbde95de2a68e7ce1f08f103fd85a681
mbernstein <mbernstein@immunitastx.com>
2023-08-17 19:08:57 +0000

    Removed unnecessary import

--- src/monkeybread/util/__init__.py
+++ src/monkeybread/util/__init__.py
@@ -1,5 +1,4 @@
 """Module for utility functions."""
 from ._neighbor_count import neighbor_count
 from ._load_merscope import load_merscope
-from ._randomize_positions import randomize_positions
 from ._subset_cells import subset_cells
