f3cea9b808761d0a10e734e48da259f24c94cebb
mbernstein <mbernstein@immunitastx.com>
2023-08-18 20:03:40 +0000

    Removed deprecated function

--- src/monkeybread/calc/__init__.py
+++ src/monkeybread/calc/__init__.py
@@ -1,6 +1,5 @@
 """Module for calculations."""
 from ._cell_neighbors import cell_neighbors, cell_neighbors_from_masks
-from ._cell_transcript_proximity import cell_transcript_proximity
 from ._cell_density import cell_density
 from ._neighborhood_profile import neighborhood_profile, cellular_niches
 from ._shortest_distances import shortest_distances, shortest_distances_pairwise
